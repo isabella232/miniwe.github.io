7cc860d937cf3bab5c38b5e63b2ca68a94a4141c
Vitaly Dryukov <miniwe@mail.ru>
2017-11-25 16:20:20 +0100

    semantic, start projects details

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
-// $(function() {
-//   $('.ui.modal').modal('show');
-// });
+$(function() {
+  // $('.ui.modal').modal('show');
+  $('.project-item').on('click', function (event) {
+    var $project = $(event.currentTarget);
+    var $target = $('.ui.modal');
+    console.log($('> .caption > a', $project));
+    $('.header', $target).html($('> .caption > a', $project).html());
+    $('.full-description', $target).html($('.full-description', $project).html());
+    $('.gists', $target).html($('> .gist-list', $project).html());
+    $target.modal('show');
+  });
+});
